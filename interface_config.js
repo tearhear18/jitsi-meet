6dae74f7faa5fa1ecc8bfda051da147950d3e834
Ramel <ramel.cabug@biomarking.com>
2020-04-25 11:40:38 +0800

    fix

--- interface_config.js
+++ interface_config.js
@@ -116,11 +116,11 @@ var interfaceConfig = {
      */
     MOBILE_APP_PROMO: true,
 
-    *
-     * Maximum coeficient of the ratio of the large video to the visible area
-     * after the large video is scaled to fit the window.
-     *
-     * @type {number}
+    // *
+    //  * Maximum coeficient of the ratio of the large video to the visible area
+    //  * after the large video is scaled to fit the window.
+    //  *
+    //  * @type {number}
      
     MAXIMUM_ZOOMING_COEFFICIENT: 1.3,
 
